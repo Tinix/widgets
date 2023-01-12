28180c3053cf92e9c473c81182a3f64030dd82e4
Daniel Tinivella <dtinivella@gmail.com>
2023-01-12 19:09:08 -0300

    You get nicely named URL helpers as well as a namespaced controller

--- app/controllers/widget_ratings_controller.rb
+++ app/controllers/widget_ratings_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 #
 # widget_ratings_controller.rb
 # Copyright (C) 2023 tinix <tinix@archlinux>
@@ -5,7 +7,14 @@
 # Distributed under terms of the MIT license.
 #
 #
-
-
-
-
+class WidgetRatingsController < ApplicationController
+  def create
+    if params[:widget_id]
+      # find the widget and update its rating
+      redirect_to widget_path(params[:widget_id]),
+                  flash[:notice] = 'Thanks for rating!'
+    else
+      head :bad_request
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :widgets, only: [:show , :index]
+  resources :widgets, only: [:show , :index] do
+    resources :ratings, only: [ :create ]
+  end
   resources :widget_ratings, only: [ :create ]
+
+	namespace :customer_service do
+		resources :widgets, only: [ :show, :update, :destroy ]
+	end
 
   get 'manufacturer/:id', to: 'manufacturers#show'
   get "/amazing", to: "widgets#index"
