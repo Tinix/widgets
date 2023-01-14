2a47a87ecdf4cdeff379c0bbcc5587addf1c710d
Daniel Tinivella <dtinivella@gmail.com>
2023-01-14 19:28:59 -0300

    add widget rating component to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def widget_ratings_component(widget, suppress_cta: false)
+    render partial: 'widgets/rating',
+           locals: { widget: widget, suppress_cta: suppress_cta }
+  end
+
+  def styled_midget_id(formatted_id)
+    content_tag(:span, formatted_id,
+                style: 'font-family: monospace')
+  end
 end
