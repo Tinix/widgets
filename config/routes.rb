5ba8e301d2d59e4738db078ce8b3fdd14d9e4d1c
Daniel Tinivella <dtinivella@gmail.com>
2023-01-17 19:14:13 -0300

    add Design System Docs

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,13 @@ Rails.application.routes.draw do
 		resources :widgets, only: [ :show, :update, :destroy ]
 	end
 
+  if Rails.env.development?
+    resources :design_system_docs, only: [:index]
+  end
+
+  # Custom routes start here
   get 'manufacturer/:id', to: 'manufacturers#show'
+
+  # Root path
   root to: "widgets#index"
 end
