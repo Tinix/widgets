66926262297fe735af5604b07f18800533505dc2
Daniel Tinivella <dtinivella@gmail.com>
2023-01-18 18:46:23 -0300

    yarn add jsdom jest babel-jest

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,3 +13,5 @@ Turbolinks.start()
 ActiveStorage.start()
 
 import "bootstrap"
+import widget_ratings from "widget_ratings"
+widget_ratings.start(window)
---
+++ app/javascript/widget_ratings/index.js
@@ -0,0 +1,43 @@
+/*
+ * index.js
+ * Copyright (C) 2023 tinix <tinix@archlinux>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+const updateUIWithRating = function(document, rating) { 
+	document.querySelectorAll("[data-rating-present]").
+    forEach((element) => { 
+      element.classList.add("db")
+      element.classList.remove("dn")
+    });
+
+	document.querySelectorAll("[data-no-rating-present]").
+    forEach((element) => { 
+      element.classList.add("dn")
+    });
+
+  document.querySelectorAll("[data-rating-label]").
+    forEach((element) => { 
+      element.innerText = `${rating}`
+    });
+}
+
+const start = function(window) {
+	const document = window.document
+	window.addEventListener("DOMContentLoaded", () => {
+		document.querySelectorAll(
+"input[type='submit'][data-rating]"
+		).forEach( (element) => {
+			element.onclick = (event) => {
+				const rating = element.value
+				event.preventDefault()
+				updateUIWithRating(document, rating)
+			}
+		});
+	});
+}
+
+module.exports = {
+	start: start
+}
