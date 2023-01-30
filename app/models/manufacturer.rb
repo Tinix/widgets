b1d4ece60a6f809f97d214bee3a15edde8a906d7
Daniel Tinivella <dtinivella@gmail.com>
2023-01-30 18:11:35 -0300

    add some point to widget and test

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Manufacturer < ApplicationRecord
+
+  # Associations
   has_many :widgets
 end
--- app/models/widget.rb
+++ app/models/widget.rb
@@ -10,4 +10,16 @@ class Widget < ApplicationRecord
   def self.fresh
     self.where(status: "fresh")
   end
+
+  def user_facing_identifier
+    id_has_string = self.id.to_s
+    if id_has_string.length < 3
+      return id_has_string
+    end
+
+    "%{first}. %{last_two}" % { 
+    first: id_has_string[0..-3],
+    last_two: id_has_string[-2..-1]
+    }
+  end
 end
